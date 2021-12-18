d4a3abd6ce8447d85e05943a9c7843cd7bc9278a
markkuope <72963505+markkuope@users.noreply.github.com>
2021-12-18 15:43:03 +0200

    Movement, Tags, Collectibles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,35 +4,73 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Change the color of the gameobject
+        // Move the gameobject
 
-        if (Input.GetKeyDown(KeyCode.Y))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            GetComponent<Renderer>().material.color = Color.yellow;
+            rb.velocity = Vector3.forward * 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            rb.velocity = Vector3.up * 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rb.velocity = Vector3.back * 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = Vector3.right * 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = Vector3.left * 10f;
+        }
+
+
+
+
+
+        // Change the color of the gameobject
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
             GetComponent<Renderer>().material.color = Color.red;
         }
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            GetComponent<Renderer>().material.color = Color.green;
-        }
         if (Input.GetKeyDown(KeyCode.B))
         {
             GetComponent<Renderer>().material.color = Color.blue;
         }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
 
+    }// update-funktio p‰‰ttyy t‰h‰n
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Collectible")
+        {
+            // Let¥s collect things
+            Destroy(collision.gameObject);
+        }
     }
 
-}
+} // Player p‰‰ttyy t‰h‰n
+
 
